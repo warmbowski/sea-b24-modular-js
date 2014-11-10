a788088c481f6a013a87668b7bd0019ba5d6e6e4
Paul Laskowski <pelaskowski@gmail.com>
2014-11-09 16:31:27 -0800

    added basic routes

---
+++ routes/courses_routes.js
@@ -0,0 +1,42 @@
+'use strict';
+var Course = require('../models/course');
+
+module.exports = function(app) {
+  app.get('/api/courses', function(req ,res) {
+    Course.find({}, function(err, data) {
+      if (err) return res.status(500).send('there was an error');
+      res.json(data);
+    });
+  });
+
+  app.get('/api/courses/:id', function(req, res) {
+    Course.findOne({'_id': req.params.id}, function(err, data) {
+      if (err) return res.status(500).send('there was an error');
+      res.json(data);
+    });
+  });
+
+  app.post('/api/courses', function(req, res) {
+    var course = new Course(req.body);
+    course.save(function(err, data) {
+      if (err) return res.status(500).send(err.errors);
+      res.json(data);
+    });
+  });
+
+  app.put('/api/courses/:id', function(req, res) {
+    var course = req.body;
+    delete course._id;
+    Course.findOneAndUpdate({'_id': req.params.id}, course, function(err, data) {
+      if (err) return res.status(500).send(err.errors);
+      res.json(data);
+    });
+  });
+
+  app.delete('/api/courses/:id', function(req, res) {
+    Course.remove({'_id': req.params.id}, function(err) {
+      if (err) return res.status(500).send('there was an error');
+      res.json({msg: 'success!'});
+    });
+  });
+};
---
+++ routes/enrollments_routes.js
@@ -0,0 +1,42 @@
+'use strict';
+var Enrollment = require('../models/enrollment');
+
+module.exports = function(app) {
+  app.get('/api/enrollments', function(req ,res) {
+    Enrollment.find({}, function(err, data) {
+      if (err) return res.status(500).send('there was an error');
+      res.json(data);
+    });
+  });
+
+  app.get('/api/enrollments/:id', function(req, res) {
+    Enrollment.findOne({'_id': req.params.id}, function(err, data) {
+      if (err) return res.status(500).send('there was an error');
+      res.json(data);
+    });
+  });
+
+  app.post('/api/enrollments', function(req, res) {
+    var enrollment = new Enrollment(req.body);
+    enrollment.save(function(err, data) {
+      if (err) return res.status(500).send(err.errors);
+      res.json(data);
+    });
+  });
+
+  app.put('/api/enrollments/:id', function(req, res) {
+    var enrollment = req.body;
+    delete enrollment._id;
+    Enrollment.findOneAndUpdate({'_id': req.params.id}, enrollment, function(err, data) {
+      if (err) return res.status(500).send(err.errors);
+      res.json(data);
+    });
+  });
+
+  app.delete('/api/enrollments/:id', function(req, res) {
+    Enrollment.remove({'_id': req.params.id}, function(err) {
+      if (err) return res.status(500).send('there was an error');
+      res.json({msg: 'success!'});
+    });
+  });
+};
